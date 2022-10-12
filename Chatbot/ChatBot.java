d0e2824d68a303a63ec793eb497e0e6400ac03ea
Podsvetka <s.y.topchiy@student.khai.edu>
2022-10-12 08:25:59 +0300

    ChatBot 1-st stage

---
+++ Chatbot/ChatBot.java
@@ -0,0 +1,10 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        String BotName = "Podsvetochka";
+        int Data = 2022;
+        System.out.println("Hello my name is " + BotName);
+        System.out.println("I was created in " + Data);
+    }
+}
