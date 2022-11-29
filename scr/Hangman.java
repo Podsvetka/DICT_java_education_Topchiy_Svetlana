0f8bb1ba1485958414e16458532e58cc7ff4c078
Podsvetka <s.y.topchiy@student.khai.edu>
2022-11-29 20:46:43 +0200

    2-nd stage Hangman

--- scr/Hangman.java
+++ scr/Hangman.java
@@ -1,7 +1,20 @@
 package scr;
 
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args){
-        System.out.println("HANGMAN" + "\nThe game will be availible soon.");
+        System.out.println("""
+                HANGMAN
+                Guess the word: >""" );
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        if(name.equals("java")){
+            System.out.println("You survived!");
+        }
+        else {
+            System.out.println("You lost!");
+        }
     }
 }
