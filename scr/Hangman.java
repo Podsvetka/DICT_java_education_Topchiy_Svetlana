aaff3fb396597d52e14554257942bcc141936c8f
Podsvetka <s.y.topchiy@student.khai.edu>
2022-11-30 01:28:45 +0200

    4-th stage Hangman

--- scr/Hangman.java
+++ scr/Hangman.java
@@ -1,19 +1,29 @@
 package scr;
 import java.util.Random;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Hangman {
     public static void main(String[] args){
-        String[] words = {"java", "python", "javascript", "kotlin"};
+        ArrayList<String> wordsArr = new ArrayList<>();
+        wordsArr.add("java");
+        wordsArr.add("kotlin");
+        wordsArr.add("python");
+        wordsArr.add("javascript");
         Random randomIntGen = new Random();
-        int randomInt = randomIntGen.nextInt(words.length);
-        String word = words[randomInt];
-        System.out.println("HANGMAN "+ "\n Guess the word:>");
-        Scanner user_input = new Scanner(System.in);
-        String answer = user_input.nextLine();
+        int randomInt = randomIntGen.nextInt(wordsArr.size());
+        String word = wordsArr.get(randomInt);
+        StringBuilder hiddenWord = new StringBuilder(word.substring(0, 2));
+        for (int i = 0; i < word.length() - 2; i++){
+            hiddenWord.append('-');
+        }
+        System.out.println("HANGMAN");
+        System.out.println("Guess the word:>" + hiddenWord );
 
-        if (answer.equals(word) ) {
+        Scanner userInput = new Scanner(System.in);
+        String answer = userInput.nextLine();
+
+        if(answer.equals(word)){
             System.out.println("You survived!");
         }
         else {
